73378237b07011c693536a98ca1a2c4ed07d8a46
Robin MASINI <robinmasini@MacBookAir.lan>
2026-06-06 17:53:39 +0200

    Adjust apple-touch-icon padding to be slightly larger

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'apple-touch-icon-v2.png'],
+      includeAssets: ['favicon.png', 'apple-touch-icon-v3.png'],
       manifest: {
         name: 'SurgiLink',
         short_name: 'SurgiLink',
