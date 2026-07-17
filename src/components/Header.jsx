8c7f0141b6dda63689674b18bd40c3bfb403e862
Robin MASINI <robinmasini@macbook-air-rm1.home>
2026-07-17 02:37:56 +0200

    fix: revert Header logos to their original 52px height

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,8 +21,8 @@ export default function Header({ title, subtitle, actions, hideTitleMobile = fal
 
                 {/* Mobile Left: Combined Logo */}
                 <div className="show-mobile" style={{ display: 'none', alignItems: 'center', gap: '8px' }}>
-                    <img src={logoSurgilink} alt="SurgiLink" style={{ height: '36px', objectFit: 'contain' }} />
-                    <img src={logoMA} alt="Medical Alliance" style={{ height: '36px', objectFit: 'contain' }} />
+                    <img src={logoSurgilink} alt="SurgiLink" style={{ height: '52px', objectFit: 'contain' }} />
+                    <img src={logoMA} alt="Medical Alliance" style={{ height: '52px', objectFit: 'contain' }} />
                 </div>
 
                 {/* Right: Actions */}
